c5aab298fe04ab77179b8a33973094b3fc6d9e0a
Howon Jung <jhwlds@gmail.com>
2026-04-02 19:27:33 -0600

    supabase setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import { propertiesRouter } from './routes/properties.js';
 import { mortgageRouter } from './routes/mortgage.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = Number(process.env.PORT ?? 3001);
 
 app.use(cors());
 app.use(express.json());
